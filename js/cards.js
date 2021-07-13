deaf76dad579de6ddc281b209db3d29352edfffc
TMP314 <t.stephen314@gmail.com>
2021-07-13 15:11:10 +0300

    Create cards.js

---
+++ js/cards.js
@@ -0,0 +1,122 @@
+var cardStats = {
+    "0": {
+        "Card-Name": "Shiva of Intemperence",
+        "Race": "Saggitarian",
+        "Type": ["Shiva Class", "Saggitarian"],
+        "Traits": ["Terrify"],
+        "Abilities": [
+            {
+                "Name": "Obliteration",
+                "Text": "At the beginning of each combat phase, you may spend 2 energy to destroy an opponents ship. Maximum 1 use per round."
+            },
+            {
+                "Name": "Consumption",
+                "Text": "After an enemy ship has been destroyed you may harvest the remains for scraps and gain 1 credit. Maximum 1 use per round."
+            }
+        ],
+        "Card-Flavour-Text": "The Saggitarian's built their Shiva Class ships for one purpose, complete annihiliation. The Shiva of Intemperence brought that to a whole new level, it had often been referred to as the ship of death.",
+        "Card-Image": "backgrounds/daniel-dociu-spacescene3.jpg",
+        "Card-Number": "Sg#001"
+        /* Taken from 
+            https://www.artstation.com/artwork/gByXQ
+            https://cdna.artstation.com/p/assets/images/images/004/586/284/4k/daniel-dociu-spacescene3.jpg?1484773235
+            Not for use, just temporary for inspiration */
+    },
+    "1": {
+        "Card-Name": "Mercurial Transporter",
+        "Race": "Orion",
+        "Type": ["Orion"],
+        "Traits": [""],
+        "Abilities": [
+            {
+                "Name": "Deployment",
+                "Text": "After this card enters play, you may also play 1 card from your hand with a cost of 2 or less credits."
+            }
+        ],
+        "Card-Flavour-Text": "One of the Orion's largest transporter ships, designed to carry a small planetary attack force inside to subjugate their enemies to their will.",
+        "Card-Image": "backgrounds/daniel-dociu-portal01.jpg",
+        "Card-Number": "Or#001"
+        /* Taken from 
+            https://www.artstation.com/artwork/gByXQ
+            https://cdnb.artstation.com/p/assets/images/images/004/727/713/large/daniel-dociu-portal01.jpg?1485828522
+            Not for use, just temporary for inspiration */
+        
+    },
+    "2": {
+        "Card-Name": "Reaper X-03",
+        "Race": "Persean",
+        "Type": ["Persean", "Reaper Class"],
+        "Traits": ["First Strike"],
+        "Abilities": [
+
+        ],
+        "Card-Flavour-Text": "The third iteration of the Persean Reaper Class, designed to reach speeds close to a percent of the speed of light for the sole purpose of annihilating their enemy ships without fear of retaliation.",
+        "Card-Image": "backgrounds/daniel-dociu-crash2.jpg",
+        "Card-Number": "Pe#001"
+        /* Taken from 
+            https://www.artstation.com/artwork/gByXQ
+            https://cdna.artstation.com/p/assets/images/images/004/726/324/4k/daniel-dociu-crash2.jpg?1485817529
+            Not for use, just temporary for inspiration */
+        
+    },
+    "3": {
+        "Card-Name": "The Virtue of Louhi",
+        "Race": "Tuonelan",
+        "Type": ["Tuonelan"],
+        "Traits": [""],
+        "Abilities": [
+            {
+                "Name": "Annihilation",
+                "Text": "Whenever The Virtue of Louhi destroys an enemy ship, you may destroy an additional enemy ship.  Maximum 1 use per round."
+            },
+            {
+                "Name": "Regenerative Coating",
+                "Text": "If The Virtue of Louhi would be destroyed, place 1 credit token on it.  When The Virtue of Louhi has 3 or more credit tokens placed on it, remove it from the game."
+            }
+        ],
+        "Card-Flavour-Text": "The flagship of the Tuonelans, responsible for the annihilation of many star systems and the only known space craft to utilise the ancient technology of the originators.",
+        "Card-Image": "backgrounds/daniel-dociu-mordyreturns.jpg",
+        "Card-Number": "Tu#001"
+        /* Taken from 
+            https://www.artstation.com/artwork/gByXQ
+            https://cdna.artstation.com/p/assets/images/images/004/726/216/4k/daniel-dociu-mordyreturns.jpg?1485816892
+            Not for use, just temporary for inspiration */
+        
+    }
+}
+
+var raceStats = {
+    "Saggitarian": {
+        "Card-Lower-Image": "backgrounds/bottom-bg0.jpg"
+    },
+    "Orion": {
+        "Card-Lower-Image": "backgrounds/bottom-bg1.jpg"
+    },
+    "Persean": {
+        "Card-Lower-Image": "backgrounds/bottom-bg2.jpg"
+    },
+    "Tuonelan": {
+        "Card-Lower-Image": "backgrounds/bottom-bg3.jpg"
+    }
+
+/*bottom-bg-
+https://www.freepik.com/kamranaydinov
+*/
+
+/*bottom-bg2-
+https://unsplash.com/photos/A_Y7b9d6CJY
+*/
+
+/*bottom-bg3-
+https://unsplash.com/photos/dLFmwIOGNYo
+*/
+
+/*bottom-bg4-
+https://unsplash.com/photos/cx90ak4WBFQ
+
+bottom-bg4-possible
+https://unsplash.com/photos/Ou8pttKRopM
+*/
+
+
+}
